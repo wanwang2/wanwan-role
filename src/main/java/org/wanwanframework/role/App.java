1dbcffc901136e06b53996c748d6924ce9f7c658
[wanwang2] <632468635@qq.com>
2017-01-22 21:48:47 +0800

    vv

---
+++ src/main/java/org/wanwanframework/role/App.java
@@ -0,0 +1,13 @@
+package org.wanwanframework.role;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
